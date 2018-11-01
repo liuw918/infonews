f3eac12d52a217494018fb6abfa10663981af494
xwp <xwp_fullstack@163.com>
2018-11-01 23:04:27 +0800

    新闻列表显示

--- info/modules/home/views.py
+++ info/modules/home/views.py
@@ -1,10 +1,13 @@
-import logging
-from flask import current_app, render_template, session
+from flask import current_app, render_template, session, request, jsonify
+from info.constants import HOME_PAGE_MAX_NEWS
 from info.models import User, News
 from info.modules.home import home_blu
+from info.utils.response_code import RET, error_map
 
 
 # 2.使用蓝图注册路由
+
+
 @home_blu.route('/')
 def index():
     # 在根路由中判断用户是否登陆
@@ -38,3 +41,46 @@ def favicon():
     # 网站小图标的请求是由浏览器自动请求，只需要实现路由即可
     # flask　中内置看返回静态文件的语法
     return current_app.send_static_file("news/favicon.ico")
+
+
+# 获取新闻列表
+@home_blu.route('/get_news_list')
+def get_news_list():
+    # 获取参数
+    cid = request.args.get("cid")
+    cur_page = request.args.get("cur_page")
+    per_cunt = request.args.get("per_count", HOME_PAGE_MAX_NEWS)
+    # 校验参数
+    if not all([cid, cur_page, per_cunt]):
+        return jsonify(errno=RET.PARAMERR, errmsg=error_map[RET.PARAMERR])
+
+    # 格式转换
+    try:
+        cid = int(cid)
+        cur_page = int(cur_page)
+        per_cunt = int(per_cunt)
+    except BaseException as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.PARAMERR, errmsg=error_map[RET.PARAMERR])
+
+    filter_list= []
+    if cid != 1:  # 不是"最新"
+        filter_list.append(News.category_id == cid)
+
+    # 根据参数查询目标新闻　　按照发布时间和分类和页码查询新闻数据
+    try:
+        pn = News.query.filter(*filter_list).order_by(News.create_time.desc()).paginate(cur_page,per_cunt)
+        news_list = [news.to_dict() for news in pn.items]
+        total_page = pn.pages
+
+    except BaseException as e:
+        current_app.loggerr.error(e)
+        return jsonify(errno=RET.DBERR, errmsg=error_map[RET.DBERR])
+
+    data = {
+        "news_list":news_list,
+        "total_page":total_page
+    }
+
+    # 将新闻封装到json返回
+    return jsonify(errno=RET.OK, errmsg=error_map[RET.OK], data=data)
